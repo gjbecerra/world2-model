dff34a7ca787d6db47ae37885101b95ff98e52b0
Patrick Kennedy <patkennedy79@gmail.com>
2016-12-20 22:31:38 -0800

    Added initial version of line chart

--- app.py
+++ app.py
@@ -13,5 +13,17 @@ def chart():
     return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 
+@app.route("/line_chart")
+def line_chart():
+    legend = 'Temperatures'
+    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
+                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
+                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
+    times = ['12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM',
+             '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
+             '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
+    return render_template('line_chart.html', values=temperatures, labels=times, legend=legend)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
