ffc28214d028ad89749d86a3a191d3b8108d432a
Patrick Kennedy <patkennedy79@gmail.com>
2016-12-25 20:39:51 -0800

    Added time chart using Moment.js

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from datetime import time
 
 
 app = Flask(__name__)
@@ -23,6 +24,33 @@ def line_chart():
              '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
              '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
     return render_template('line_chart.html', values=temperatures, labels=times, legend=legend)
+
+
+@app.route("/time_chart")
+def time_chart():
+    legend = 'Temperatures'
+    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
+                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
+                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
+    times = [time(hour=11, minute=14, second=15),
+             time(hour=11, minute=14, second=30),
+             time(hour=11, minute=14, second=45),
+             time(hour=11, minute=15, second=00),
+             time(hour=11, minute=15, second=15),
+             time(hour=11, minute=15, second=30),
+             time(hour=11, minute=15, second=45),
+             time(hour=11, minute=16, second=00),
+             time(hour=11, minute=16, second=15),
+             time(hour=11, minute=16, second=30),
+             time(hour=11, minute=16, second=45),
+             time(hour=11, minute=17, second=00),
+             time(hour=11, minute=17, second=15),
+             time(hour=11, minute=17, second=30),
+             time(hour=11, minute=17, second=45),
+             time(hour=11, minute=18, second=00),
+             time(hour=11, minute=18, second=15),
+             time(hour=11, minute=18, second=30)]
+    return render_template('time_chart.html', values=temperatures, labels=times, legend=legend)
 
 
 if __name__ == "__main__":
